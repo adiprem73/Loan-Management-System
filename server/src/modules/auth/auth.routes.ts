7dd25ffe6d005585d4d0baa47c5674d2d99c5663
Aditya Prem <aditya.prem2003@gmail.com>
2026-04-30 13:20:18 +0530

    amdin functionalities added successfully

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import * as authController from './auth.controller';
 import { protect } from '../../middleware/auth.middleware';
+import { authorize } from '../../middleware/rbac.middleware';
 
 const router = Router();
 
@@ -8,7 +9,7 @@ console.log("aditya prem")
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/me', protect, authController.getMe);
-router.get('/users', authController.getAllUsers);
+router.get('/users', protect, authorize('admin'), authController.getAllUsers);
 router.get('/test123', (req, res) => {
     res.send('WORKING BRO');
   });
